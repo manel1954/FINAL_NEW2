e2a0bd15365985ffa6c9f6972abb7f3d1f924fd7
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-05 16:17:43 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -29,7 +29,7 @@ fi
 usuario="/home/pi"
 usuario="$usuario"
 fecha_imagen=$(awk "NR==1" /home/pi/version-fecha-actualizacion)
-nombre_imagen="FINAL"
+nombre_imagen="NEW2-"
 version=$nombre_imagen$fecha_imagen
 
 
