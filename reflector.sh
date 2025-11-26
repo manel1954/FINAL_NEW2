522b0414373c870d7f3dc671c42937706721147c
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-26 20:12:30 +0100

    1

--- reflector.sh
+++ reflector.sh
@@ -8,28 +8,20 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-			
-			
-			
-			
-			# cd /home/pi/YSFClients/YSFGateway
-			#sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php 
-            
-			
-			#curl https://hostfiles.refcheck.radio/YSFHosts.txt > /home/pi/YSFClients/YSFGateway/YSFHosts.txt
+			#cp /home/pi/A108/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+			#sleep 3			
+			#clear
+			#echo "${VERDE}***********************************************"
+			#		echo "*  ACTUALIZANDO REFLECTORES YSF               *"
+			#		echo "***********************************************"
+			#sleep 3
 			
 			cd /home/pi/
-wget --user-agent="YSFGateway" https://hostfiles.refcheck.radio/YSFHosts.txt
-#sudo cp /home/pi/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
-#sudo chmod 777 /home/pi/YSFClients/YSFGateway/YSFHosts.txt
-
-sleep 20
-
-sudo mv /home/pi/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
-			sleep 3
-	read a		
+            wget --user-agent="YSFGateway" https://hostfiles.refcheck.radio/YSFHosts.txt
+            sudo mv /home/pi/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+			sleep 3		
 			clear
-			echo "${VERDE}**********************************"
+			echo "${VERDE}**********************************"       
 					echo "*  ACTUALIZANDO REFLECTORES YSF  *"
 					echo "**********************************"
 			sleep 3
