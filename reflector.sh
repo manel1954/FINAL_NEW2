8390dcc7cb568b61c7792425ee40a5d6bc52725f
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-11-26 19:48:14 +0100

    Update reflector.sh

--- reflector.sh
+++ reflector.sh
@@ -18,9 +18,10 @@ MARRON="\33[38;5;138m"
 			
 			#curl https://hostfiles.refcheck.radio/YSFHosts.txt > /home/pi/YSFClients/YSFGateway/YSFHosts.txt
 			
-			#
+			sudo rm /home/pi/YSFHosts.txt
 wget --user-agent="YSFGateway" https://hostfiles.refcheck.radio/YSFHosts.txt
-cp /home/pi/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+sudo cp /home/pi/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+sudo chmod 777 /home/pi/YSFClients/YSFGateway/YSFHosts.txt
 
 			sleep 3
 			
